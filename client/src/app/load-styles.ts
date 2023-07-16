89295091b8ddea1ff0550fee048923650a0f4f08
KristofferRoenLie <KristofferRoenLie@gmail.com>
2023-07-16 20:59:03 +0200

    Moved clock application.

---
+++ client/src/app/load-styles.ts
@@ -0,0 +1,16 @@
+export const loadStyles = (url: string, links: {id: string; href: string;}[]) => {
+	links.forEach(style => {
+		if (document.head.querySelector('#' + style.id))
+			return;
+
+		const linkEl = document.createElement('link');
+		linkEl.id = style.id;
+		linkEl.rel = 'stylesheet';
+		linkEl.href = (url + '/' + style.href)
+			.replaceAll(location.origin, '')
+			.replaceAll(/\/+/g, '/')
+			.replaceAll(/\/$/g, '');
+
+		document.head.appendChild(linkEl);
+	});
+};
